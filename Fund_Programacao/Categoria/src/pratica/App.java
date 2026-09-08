8ba62147e901cbb323a9323829a63ed5d53f588d
Michelder Belo <mbelo2003@gmail.com>
2026-09-08 16:23:41 -0300

    Update App.java

--- Fund_Programacao/Categoria/src/pratica/App.java
+++ Fund_Programacao/Categoria/src/pratica/App.java
@@ -12,13 +12,13 @@ public class App {
 		
 		if (idade < 5) {
 			System.out.println("Idade Inválida ");
-		}else if (idade >= 5 && idade <= 7) {
+		}else if (idade <= 7) {
 			System.out.println("Categoria Infantil");
-		}else if (idade >= 8 && idade <= 10) {
+		}else if (idade <= 10) {
 			System.out.println("Categoria Juvenil");
-		}else if(idade >= 11 && idade <= 15) {
+		}else if(idade <= 15) {
 			System.out.println("Categoria Adolescente");
-		}else if(idade >= 16 && idade <=30) {
+		}else if(idade <=30) {
 			System.out.println("Categoria Adulto");
 		}else if(idade > 30) {
 			System.out.println("Categoria Sênior");
